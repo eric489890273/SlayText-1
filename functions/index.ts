f1a0987a34379857fa43c9c239276efc9dc8fac0
eric48980273 <eric970531@gmail.com>
2025-09-25 23:37:44 +0800

    9252

--- functions/index.ts
+++ functions/index.ts
@@ -1,13 +1,6 @@
-import { Hono } from "hono";
-import { createAdaptor } from "@hono/express";
-import app from "../server/index";
-
-const hono = new Hono();
-const expressHandler = createAdaptor(app);
-
-// 所有請求都交給 Express 處理
-hono.all("*", async (c) => {
-  return expressHandler(c.req.raw, c.env);
-});
-
-export default hono;
+// functions/index.ts
+export async function onRequest(context) {
+  return new Response('Hello from Worker!', {
+    headers: { 'Content-Type': 'text/plain' }
+  })
+}
